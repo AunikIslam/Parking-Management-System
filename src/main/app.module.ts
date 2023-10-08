5235347f8fc5d2cdda6ee1df8bb4881db7d392c1
Mezbaul Islam Aunik <47604952+AunikIslam@users.noreply.github.com>
2023-10-08 12:15:57 +0600

    1. App component files added 2. Angular Material Added 3. Required Dependencies Installed

---
+++ src/main/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
